c944976946fac77b729cd6ba1c98b5854b51f99b
jefersonrode <jeferson.rode@unidavi.edu.br>
2020-11-11 21:41:58 -0300

    modificação no css

--- Aulas/Novembro/calculadoraphp/calcv4.php
+++ Aulas/Novembro/calculadoraphp/calcv4.php
@@ -38,96 +38,113 @@ if (isset($_POST["reset"])) {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>RoDesign</title>
+    <title>Calculadora PHP</title>
     <style type="text/css">
       td {
-      color: #FFFF00;
-      font-weight: bold;
-      text-align: center;
-      border: 1px solid #000099;
+         text-align: center;
+         border: 1px solid red;
+         height: 50px;
       }
-      input#calc {
-      background-color: #759475;
-      text-align: right;
-      color: #222722;
-      border-color: #000099;
+      #bt {
+         height: 40px;
+         width: 35px;
+         font-size: 20px;
+         color: red;
+         background-color: black;
+         border-radius: 3px;
+         border: 2px solid grey;
+      }
+      input#v1 {
+         background-color: #759475;
+         text-align: right;
+         font-size: 20px;
+         color: #222722;
+         border-color: grey;
+         height: 30px;
       }
       table#calculadora {
-      border: 1px solid  #000099;
-      background-color: #3300cc;
+         border: 14px solid  black;
+         border-radius: 8px;
+         background-color: black;
       }
-      #titulo {
-      font-family: arial, verdana, sans serif;
-      font-size: 1em;
-      font-weight: bold;
-      text-align: center;
+      #lab1 {
+         color: grey;
+         font-family: Arial, Helvetica, sans-serif;
       }
-      body{background-color: #f9f9f9; padding: 35px;} 
+      body{
+         background-color: #f9f9f9;
+          padding: 35px;
+      } 
    </style>
   </head>
+
 <body>
-<label>Calculadora PHP</label>
 <form method="POST">
    <table id="calculadora">
       <tr>
          <td colspan="4">
-            <input id="v1" type="text" name="v1" value="<?=$_SESSION["v1"]?>" /><br>
+         <label id="lab1">CALCULADORA PHP</label>
+         </td>
+      </tr>
+      <tr>
+         <td colspan="4">
+            <input id="v1" type="text" name="v1" value="<?=$_SESSION["v1"]?>" />
          </td>
       </tr>
       <tr>
          <td>
-            <input type="submit" name="num[]" value="7" />
+            <input id="bt" type="submit" name="num[]" value="7" />
          </td>
          <td>
-            <input type="submit" name="num[]" value="8" />
+            <input id="bt" type="submit" name="num[]" value="8" />
          </td>
          <td>
-         <input type="submit" name="num[]" value="9" />
+         <input id="bt" type="submit" name="num[]" value="9" />
          </td>
          <td>
-            <input type="submit" name="num[]" value="/" />
+            <input id="bt" type="submit" name="num[]" value="/" />
          </td>
       </tr>
          <tr>
          <td>
-            <input type="submit" name="num[]" value="4" />
+            <input id="bt" type="submit" name="num[]" value="4" />
          </td>
       <td>
-            <input type="submit" name="num[]" value="5" />
+            <input id="bt" type="submit" name="num[]" value="5" />
       </td>
       <td>
-            <input type="submit" name="num[]" value="6" />
+            <input id="bt" type="submit" name="num[]" value="6" />
       </td>
       <td>
-            <input type="submit" name="num[]" value="*" />
+            <input id="bt" type="submit" name="num[]" value="*" />
       </td>
       </tr>
       <tr>
          <td>
-            <input type="submit" name="num[]" value="1" />
+            <input id="bt" type="submit" name="num[]" value="1" />
          </td>
          <td>
-            <input type="submit" name="num[]" value="2" />
+            <input id="bt" type="submit" name="num[]" value="2" />
          </td>
          <td>
-            <input type="submit" name="num[]" value="3" />
+            <input id="bt" type="submit" name="num[]" value="3" />
          </td>
          <td>
-            <input type="submit" name="num[]" value="-" />
+            <input id="bt" type="submit" name="num[]" value="-" />
          </td>
       </tr>
       <tr>
          <td>
-            <input type="submit" id="reset" name="reset" value="C">
+            <input id="bt" type="submit" name="reset" value="C">
          </td>
          <td>
-            <input type="submit" name="num[]" value="0" />
+            <input id="bt" type="submit" name="num[]" value="0" />
          </td>
          <td>
-            <input type="submit" name="calcular" value="=" />
+            <input id="bt" type="submit" name="calcular" value="=" />
          </td>
          <td>
-            <input type="submit" name="num[]" value="+" />
+            <input id="bt" type="submit" name="num[]" value="+" />
          </td>
       </tr>
    </table>
